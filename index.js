d7e4da1cda33af62ab7e921a18984572ca01059b
Sumaiya Sinja <sumiyasinja@gmail.com>
2023-12-21 11:41:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,111 @@
+const express = require('express')
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to your automotive shop server!')
+})
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ctrkbrk.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("TaskManagerDB");
+    const TaskCollection = database.collection("TaskCollection");
+   
+    // Read all tasks
+    app.get('/tasks', async(req,res)=>{  
+      const result = await TaskCollection.find().toArray();
+      res.send(result);
+    })
+    // post tasks
+    app.post('/tasks', async(req,res)=>{
+        const newCar= req.body
+        console.log(newCar)
+        const result = await TaskCollection.insertOne(newCar);
+        res.send(result)
+      })
+
+      // get single product
+    app.get('/tasks/:id', async (req, res) => {
+          const id = req.params.id
+          const filter = { _id: new ObjectId(id) }
+          const result = await TaskCollection.findOne(filter); 
+          res.send(result);
+        })
+        
+    // update by id
+    app.put('/tasks/:id', async (req, res) => {
+      const id = req.params.id;
+      const car = req.body;
+      console.log("Body", id, car);
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedCar ={
+        $set: {
+         name : car.name,
+         price : car.price,
+         rating : car.rating,
+         brands : car.brands,
+         types : car.types,
+         description : car.description,
+         photo : car.photo,
+
+      },
+    };
+      const result = await TaskCollection.updateOne(
+        filter,
+        updatedCar,
+        options
+      );
+      res.send(result);
+    });
+
+
+    //delete
+
+       
+          app.delete('/tasks/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await TaskCollection.deleteOne(query);
+            res.send(result);
+        })
+          
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+  } finally {
+ 
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+  console.log(`Your task manager server is listening on port ${port}`)
+})
